7bbcd31ce2c7d7f1f807ff2b39fc99032fc08e40
Qrow <nachoesposito@gmail.com>
2024-10-01 18:45:46 +0200

    Primer boceto

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}', // Asegúrate de incluir Astro y otros formatos relevantes.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
