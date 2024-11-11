8230f89d159bf839f9ac5699b3b75d2f9dfc5e15
andreamagan <andrea.magan@outlook.com>
2024-11-11 23:07:03 +0100

    fix: try astro config to deploy web

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://Gallegarmy.github.io',
-  base: 'LareiraConf-MainPage',
+  site: "https://Gallegarmy.github.io",
+  base: ".",
   integrations: [tailwind()],
 });
