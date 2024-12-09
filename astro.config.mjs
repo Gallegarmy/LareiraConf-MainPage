1a4b7bc6fff6144c715187429c84086dc073694a
Néstor <nestor@nstlopez.com>
2024-12-09 21:59:43 +0100

    feat: more improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import compress from "astro-compress";
+
 export default defineConfig({
   site: "https://www.lareiraconf.es",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), compress()],
 });
