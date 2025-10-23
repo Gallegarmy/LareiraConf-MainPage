6a92237793bcda6843262e45406e0da172108918
tizianaamica <tiziana.amicarella@unet.edu.ve>
2025-10-23 17:09:23 +0200

    fix: :ambulance: arreglo bug vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
-    allowedHosts: ["lareiraconf.es"],
+    allowedHosts: ["lareiraconf.es", "all"],
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 4321,
   },
 });
