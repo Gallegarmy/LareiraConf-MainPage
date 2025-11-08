ade0245492d5a0e19ab69073aee236ae2b4f0dad
andreamagan <andrea.magan@outlook.com>
2025-11-08 18:38:16 +0100

    feat: first section

---
+++ src/components/Home/HomeSection.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "@styles/home.css";
+
+const HomeSection = () => {
+  return (
+    <div className="panel home-section">
+      <div className="content">
+        <h1>LareiraConf</h1>
+        <h2>2026</h2>
+        <p>12 y 13 de Diciembre | A Coruña</p>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSection;
