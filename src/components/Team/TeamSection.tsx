a3dcef05799990dfa2183d2d22b4da1a6d140154
andreamagan <andrea.magan@outlook.com>
2025-11-29 16:15:23 +0100

    feat: improve team copy

--- src/components/Team/TeamSection.tsx
+++ src/components/Team/TeamSection.tsx
@@ -229,11 +229,16 @@ const TeamSection: React.FC = () => {
         <div className="team-copy">
           <h2>El equipo</h2>
           <p>
-            Somos un grupo de apasionados por la tecnología unidos por un
-            objetivo común: crear espacios donde las comunidades locales puedan
-            conectarse y compartir ideas. Así nació Lareiraconf, un proyecto que
-            comenzó de una chispa en unas admincañas y que nos ha llevado hasta
-            aquí.
+            Somos un grupo de personas que disfruta de la tecnología y, sobre
+            todo, de la gente que se junta alrededor de ella. Unidos por un
+            objetivo común: crear un evento para juntarse, conectar y compartir
+           ideas.
+          </p>
+          <p>
+            Lo hacemos de forma voluntaria y sin ánimo de lucro, dedicando
+            tiempo y cariño a cada detalle. Porque creemos en la comunidad
+            tecnológica de Galicia y en el poder de reunirnos alrededor de la
+            Lareira.
           </p>
         </div>
 
