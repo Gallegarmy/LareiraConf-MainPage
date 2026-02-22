ebdf5d04dc8d135b6d6551df712b3508630419f2
andreamagan <andrea.magan@outlook.com>
2026-02-22 21:24:44 +0100

    feat(communities): add communities section to menu and pages
    - Add communities link with flag icon to header nav (after team)
    - Internationalize nav link using i18n (es/gl)
    - Replace placeholder logos with real community images from img/communities
    - Add GDG Coruña community
    - Remove card wrapper boxes, enlarge images with flex-wrap grid
    - Add communities section to gl/index.astro
    - Internationalize section title, description and aria labels (es/gl)

--- src/components/Communities/communities-data.ts
+++ src/components/Communities/communities-data.ts
@@ -1,4 +1,14 @@
-import placeholderLogo from "@img/icons/shield.svg";
+import wtfLogo from "@img/communities/all_flag-wtf.png";
+import sysarmyLogo from "@img/communities/all_flag-sysarmy.png";
+import pythonLogo from "@img/communities/all_flag-python.png";
+import gpulLogo from "@img/communities/all_flag-gpul.png";
+import bricolabsLogo from "@img/communities/all_flag-bricolabs.png";
+import datolaLogo from "@img/communities/all_flag-datola.png";
+import wpLogo from "@img/communities/all_flag-wp.png";
+import exfLogo from "@img/communities/all_flag-exf.png";
+import jugLogo from "@img/communities/all_flag-jug.png";
+import aquelarreLogo from "@img/communities/all_flag-aquelarre.png";
+import gdgLogo from "@img/communities/all_flag-gdg.png";
 
 export interface Community {
   id: string;
@@ -13,70 +23,77 @@ export const communities: Community[] = [
     id: "corunha-wtf",
     name: "CoruñaWTF",
     url: "https://corunhawtf.com",
-    logo: placeholderLogo.src,
+    logo: wtfLogo.src,
     alt: "Logo de CoruñaWTF",
   },
   {
     id: "sysarmy-galicia",
     name: "SysarmyGalicia",
     url: "https://sysarmy.com/es/",
-    logo: placeholderLogo.src,
+    logo: sysarmyLogo.src,
     alt: "Logo de SysarmyGalicia",
   },
   {
     id: "python-corunha",
     name: "Python Coruña",
     url: "https://www.meetup.com/python-coruna/",
-    logo: placeholderLogo.src,
+    logo: pythonLogo.src,
     alt: "Logo de Python Coruña",
   },
   {
     id: "gpul",
     name: "GPUL",
     url: "https://gpul.org/",
-    logo: placeholderLogo.src,
+    logo: gpulLogo.src,
     alt: "Logo de GPUL",
   },
   {
     id: "bricolabs",
     name: "Bricolabs",
     url: "https://bricolabs.cc/",
-    logo: placeholderLogo.src,
+    logo: bricolabsLogo.src,
     alt: "Logo de Bricolabs",
   },
   {
     id: "datola",
     name: "Datola",
     url: "https://datola.es/",
-    logo: placeholderLogo.src,
+    logo: datolaLogo.src,
     alt: "Logo de Datola",
   },
   {
     id: "wordpress",
     name: "WordPress Coruña",
     url: "https://www.meetup.com/wordpress-coruna/",
-    logo: placeholderLogo.src,
+    logo: wpLogo.src,
     alt: "Logo de WordPress Coruña",
   },
   {
     id: "enxeneria-sen-fronteiras",
     name: "Enxeñería Sen Fronteiras",
     url: "https://esf-galicia.org/",
-    logo: placeholderLogo.src,
+    logo: exfLogo.src,
     alt: "Logo de Enxeñería Sen Fronteiras",
   },
   {
     id: "corunha-jug",
     name: "CoruñaJUG",
     url: "https://www.meetup.com/coruna-jug/",
-    logo: placeholderLogo.src,
+    logo: jugLogo.src,
     alt: "Logo de CoruñaJUG",
   },
   {
     id: "aquelarre-dixital",
     name: "Aquelarre Dixital",
     url: "https://aquelarredixital.gal/",
-    logo: placeholderLogo.src,
+    logo: aquelarreLogo.src,
     alt: "Logo de Aquelarre Dixital",
+  },
+  {
+    id: "gdg-corunha",
+    name: "GDG Coruña",
+    url: "https://gdg.community/gdg-coruna/",
+    logo: gdgLogo.src,
+    alt: "Logo de GDG Coruña",
   },
 ];
